0b35d96322ebaaff075830ff27616ca74591b6d6
Teemu Harju <teemu.harju@gmail.com>
2015-04-20 13:48:55 +0300

    Support for chat and inbox API endpoints.

--- lib/flowdock_logger.ex
+++ lib/flowdock_logger.ex
@@ -5,7 +5,8 @@ defmodule Logger.Backends.Flowdock do
   def init(_) do
     if user = Process.whereis(:user) do
       Process.group_leader(self, user)
-      {:ok, configure()}
+      conf = configure()
+      {:ok, conf}
     else
       {:error, :ignore}
     end
@@ -15,9 +16,9 @@ defmodule Logger.Backends.Flowdock do
     {:ok, state}
   end
   
-  def handle_event({level, _gl, {Logger, msg, ts, md}}, %{level: min_level} = state) do
+  def handle_event({level, _gl, {Logger, msg, ts, md}}, %{level: min_level, endpoint: endpoint} = state) do
     if is_nil(min_level) or Logger.compare_levels(level, min_level) != :lt do
-      log_event(level, msg, ts, md, state)
+      log_event(endpoint, level, msg, ts, md, state)
     end
     {:ok, state}
   end
@@ -38,31 +39,33 @@ defmodule Logger.Backends.Flowdock do
     source = Keyword.get(env, :source)
     from_address = Keyword.get(env, :from_address)
     subject = Keyword.get(env, :subject)
-    from_name = Keyword.get(env, :from_name, Node.self)
+    from_name = Keyword.get(env, :from_name, to_string(Node.self))
     project = Keyword.get(env, :project)
     tags = Keyword.get(env, :tags)
+
+    endpoint = Keyword.get(env, :endpoint, :chat)
     
     %{format: format, level: level, metadata: metadata, api_token: api_token,
       source: source, subject: subject, from_name: from_name, project: project,
-      tags: tags, from_address: from_address}
+      tags: tags, from_address: from_address, endpoint: endpoint}
   end
   
-  defp log_event(level, msg, ts, md, %{format: format, metadata: metadata} = state) do
+  defp log_event(:inbox, level, msg, ts, md, %{format: format, metadata: metadata} = state) do
     ansidata = Logger.Formatter.format(format, level, msg, ts, Dict.take(md, metadata))
     
     url = "https://api.flowdock.com/v1/messages/team_inbox/#{state.api_token}"
     headers = [{"Content-Type",  "application/json"}]
     
     payload = ~s(
-    {
-      "source": "#{state.source}",
-      "from_address": "#{state.from_address}",
-      "subject": "#{state.subject}",
-      "content": "#{ansidata}",
-      "from_name": "#{state.from_name}",
-      "project": "#{state.project}",
-      "tags": #{inspect state.tags}
-    })
+      {
+          "source": "#{state.source}",
+          "from_address": "#{state.from_address}",
+          "subject": "#{state.subject}",
+          "content": "#{ansidata}",
+          "from_name": "#{state.from_name}",
+          "project": "#{state.project}",
+          "tags": #{inspect state.tags}
+      })
     
     {:ok, status_code, _resp_headers, client_ref} =
       :hackney.request(:post, url, headers, payload, [])
@@ -70,6 +73,30 @@ defmodule Logger.Backends.Flowdock do
     if status_code == 200 do
       {:ok, _body} = :hackney.body(client_ref)      
     end
+    
+  end
+
+  defp log_event(:chat, level, msg, ts, md, %{format: format, metadata: metadata} = state) do
+    ansidata = Logger.Formatter.format(format, level, msg, ts, Dict.take(md, metadata))
+    
+    url = "https://api.flowdock.com/v1/messages/chat/#{state.api_token}"
+    headers = [{"Content-Type",  "application/json"}]
+    
+    payload = ~s(
+      {
+          "content": "#{ansidata}",
+          "external_user_name": "#{String.replace(state.project, " ", "_")}",
+          "tags": #{inspect state.tags}
+          
+      })
+    
+    {:ok, status_code, _resp_headers, client_ref} =
+      :hackney.request(:post, url, headers, payload, [])
+    
+    if status_code == 200 do
+      {:ok, _body} = :hackney.body(client_ref)
+    end
+    
   end
   
 end
--- mix.exs
+++ mix.exs
@@ -12,7 +12,7 @@ defmodule FlowdockLogger.Mixfile do
   #
   # Type `mix help compile.app` for more information
   def application do
-    [applications: [:logger, :hackney]]
+    [applications: [:hackney, :logger]]
   end
 
   # Dependencies can be Hex packages:
